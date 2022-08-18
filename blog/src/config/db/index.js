e2576d11c929b18369d34b2cc2ea0b03a4636b9d
MisterTino <deezaymisstertino@gmail.com>
2022-08-18 07:37:38 +0700

    commit html css js

--- blog/src/config/db/index.js
+++ blog/src/config/db/index.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 async function connect(){
     try{
-        await mongoose.createConnection('mongodb://localhost:27017/blog_education_dev');
+        await mongoose.createConnection('mongodb://localhost:27017/blog_education_dev', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
         console.log('Connected to MongoDB');
     }
     catch(e){
